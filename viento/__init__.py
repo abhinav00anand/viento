a151e9655e1cba150c6c422f5c68cfcc97f9dd91
abhinav anand <280037518+abhinav00anand@users.noreply.github.com>
2026-08-14 18:27:23 +0530

    chore: add node_name to ZephyrConfig and bump version to 0.2.6

--- viento/__init__.py
+++ viento/__init__.py
@@ -18,7 +18,7 @@ Run as a node:
 Documentation: https://github.com/abhinav00anand/zephyr
 """
 
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 __author__ = "Zephyr Cloud Team"
 __email__ = "indrohelpdesk@gmail.com"
 __license__ = "MIT"
