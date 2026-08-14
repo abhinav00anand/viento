d8a49c87fb6b4ded308b0e87d3da7ebec194e0b1
abhinav anand <280037518+abhinav00anand@users.noreply.github.com>
2026-08-14 18:37:12 +0530

    chore: fix HardwareStats mapping inside ConnectionManager._perform_handshake and bump version to 0.2.8

--- viento/__init__.py
+++ viento/__init__.py
@@ -18,7 +18,7 @@ Run as a node:
 Documentation: https://github.com/abhinav00anand/zephyr
 """
 
-__version__ = "0.2.7"
+__version__ = "0.2.8"
 __author__ = "Zephyr Cloud Team"
 __email__ = "indrohelpdesk@gmail.com"
 __license__ = "MIT"
